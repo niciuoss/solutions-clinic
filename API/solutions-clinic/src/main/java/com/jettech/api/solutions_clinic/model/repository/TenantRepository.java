7eabca6e68e37a705e431ebef171def6a9748241
thiagomarinhob <thiagomarinhobezerra@gmail.com>
2026-01-10 21:44:21 -0300

    add job verify trial expiration

--- API/solutions-clinic/src/main/java/com/jettech/api/solutions_clinic/model/repository/TenantRepository.java
+++ API/solutions-clinic/src/main/java/com/jettech/api/solutions_clinic/model/repository/TenantRepository.java
@@ -4,7 +4,10 @@ import com.jettech.api.solutions_clinic.model.entity.Tenant;
 import com.jettech.api.solutions_clinic.model.entity.TenantStatus;
 import com.jettech.api.solutions_clinic.model.entity.TypeTenant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,5 +25,11 @@ public interface TenantRepository extends JpaRepository<Tenant, UUID> {
     List<Tenant> findByStatus(TenantStatus status);
     
     List<Tenant> findByStatusAndActive(TenantStatus status, boolean active);
+
+    @Query("SELECT t FROM tenant t WHERE t.status = :status AND t.planType = 'TRIAL' AND t.trialEndsAt < :now")
+    List<Tenant> findExpiredTrials(
+            @Param("status") TenantStatus status,
+            @Param("now") LocalDate now
+    );
 }
 
